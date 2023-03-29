5aec8bd86a55edcf3109103b8b6f5e29ca4703b6
HenPadenis <henriqueptho44@hotmail.com>
2023-03-29 14:39:53 -0300

    commit dia 29/03

--- DataGas/javascript/graficoBom.js
+++ DataGas/javascript/graficoBom.js
@@ -1,126 +1,138 @@
-
-var valores = [15, 9, 5, 5, 2, 5, 8, 9, 10, 12, 15, 5];
-var mediab1 = document.querySelector(".mediab1");
-var calculoMedia;
-
-var chartDom = document.getElementById('grafico-bom');
-var myChart = echarts.init(chartDom, 'dark');
-var option;
+// var mediab1 = document.querySelector(".mediab1");
+// var calculoMedia;
+// var valores = [13, 9, 5, 15, 11, 5, 8, 9, 10, 12, 10, 5];
+// var chartDom = document.getElementById('grafico-bom');
+// var myChart = echarts.init(chartDom, 'dark');
+// var option;
 
 
-const colorsDark = ['rgba(255, 160, 30, 1)', 'rgba(50, 164, 255, 1)', 'rgba(46,209,0,1)'];
-const colorsLight = [];
+// const colorsDark = ['rgba(255, 160, 30, 1)', 'rgba(0, 100, 255, 1)', 'rgba(46,209,0,1)'];
+// const colorsLight = [];
 
-option = {
-  backgroundColor: '',
-  color: colorsDark,
-  toolbox: {
-    top: 20,
-    right: 105,
-    show: true,
-    feature: {
-      dataZoom: {
-        yAxisIndex: 'none'
-      },
-      dataView: { readOnly: false },
-      magicType: { type: ['line', 'bar', 'stack'] },
-      saveAsImage: {}
-    }
-  },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross',
-    }
-  },
-  legend: {
-    top: 25,
-    textStyle: {color: 'white'},
-  },
-  grid: {
-    top: 70,
-    bottom: 50
-  },
-  xAxis: [
-    {
-      type: 'category',
-      boundaryGap: false,
-      axisTick: {
-        alignWithLabel: true
-      },
-      axisLine: {
-        lineStyle: {
-        color: colorsDark[1]
-        }
-      },
-      data: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
-    },
-  ],
-  yAxis: [
-    {
-      type: 'value',
-      axisLabel: {
-        formatter: '{value} %'
-      }
-    }
-  ],
-  series: [
-    {
-      color: colorsDark[1],
-      name: 'Gás 1',
-      type: 'line',
-      markPoint: {
-        data: [
-          { type: 'max', name: 'Max' },
-        ]
-      },
-      smooth: true,
-      emphasis: {
-        focus: 'series'
-      },
-      data: valores,
-    },
-    /*
-    {
-      name: 'Gás 2',
-      type: 'line',
-      markPoint: {
-        data: [
-          { type: 'max', name: 'Max' },
-        ]
-      },
-      smooth: true,
-      emphasis: {
-        focus: 'series'
-      },
+// option = {
+//   backgroundColor: '',
+//   color: colorsDark,
+//   toolbox: {
+//     top: 20,
+//     right: 105,
+//     itemSize: 20,
+//     show: true,
+//     feature: {
+//       dataZoom: {
+//         yAxisIndex: 'none'
+//       },
+//       dataView: { readOnly: false },
+//       magicType: { type: ['line', 'bar', 'stack'] },
+//       saveAsImage: {}
+//     }
+//   },
+//   tooltip: {
+//     trigger: 'axis',
+//     axisPointer: {
+//       type: 'cross',
+//     }
+//   },
+//   legend: {
+//     top: 25,
+//     textStyle: {color: 'white'},
+//   },
+//   grid: {
+//     top: 70,
+//     bottom: 50
+//   },
+//   xAxis: [
+//     {
+//       type: 'category',
+//       boundaryGap: false,
+//       axisTick: {
+//         alignWithLabel: true
+//       },
+//       axisLine: {
+//         lineStyle: {
+//         color: colorsDark[1]
+//         }
+//       },
+//       data: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+//     },
+//   ],
+//   yAxis: [
+//     {
+//       type: 'value',
+//       axisLabel: {
+//         formatter: '{value} %'
+//       }
+//     }
+//   ],
+//   series: [
+//     {
+//       symbolSize: 7,
+//       color: colorsDark[1],
+//       name: 'Gás 1',
+//       type: 'line',
+//       markPoint: {
+//         data: [
+//           { type: 'max', name: 'Max', symbolSize: 40 },
+//         ]
+//       },
+//       smooth: true,
+//       emphasis: {
+//         focus: 'series'
+//       },
+//       areaStyle: {
+//         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+//           {
+//             offset: 0,
+//             color: 'rgba(58,77,233,0.8)'
+//           },
+//           {
+//             offset: 1,
+//             color: 'rgba(58,77,233,0.3)'
+//           }
+//         ])
+//       },
+//       data: valores,
+//     },
+//     /*
+//     {
+//       name: 'Gás 2',
+//       type: 'line',
+//       markPoint: {
+//         data: [
+//           { type: 'max', name: 'Max' },
+//         ]
+//       },
+//       smooth: true,
+//       emphasis: {
+//         focus: 'series'
+//       },
       
-      data: [
-        3.9, 5.9, 11.1, 18.7, 48.3, 69.2, 231.6, 46.6, 55.4, 18.4, 10.3, 0.7
-      ]
+//       data: [
+//         3.9, 5.9, 11.1, 18.7, 48.3, 69.2, 231.6, 46.6, 55.4, 18.4, 10.3, 0.7
+//       ]
       
-    },
-    {
-      name: 'Gás 3',
-      type: 'line',
-      markPoint: {
-        data: [
-          { type: 'max', name: 'Max' },
-        ]
-      },
-      smooth: true,
-      emphasis: {
-        focus: 'series'
-      },
+//     },
+//     {
+//       name: 'Gás 3',
+//       type: 'line',
+//       markPoint: {
+//         data: [
+//           { type: 'max', name: 'Max' },
+//         ]
+//       },
+//       smooth: true,
+//       emphasis: {
+//         focus: 'series'
+//       },
       
       
-      data: [
-        2.9, 4.9, 10.1, 45.7, 0, 220, 231.6, 46.6, 55.4, 18.4, 10.3, 0.7
-      ]
+//       data: [
+//         2.9, 4.9, 10.1, 45.7, 0, 220, 231.6, 46.6, 55.4, 18.4, 10.3, 0.7
+//       ]
       
-    },
-    */
+//     },
+//     */
     
-  ]
-};
+//   ]
+// };
 
-option && myChart.setOption(option);
+// option && myChart.setOption(option);
--- DataGas/javascript/script.js
+++ DataGas/javascript/script.js
@@ -1,35 +1,48 @@
-/*var nivel;
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "http://localhost:8080/data-gas");
-xmlhttp.send();
-xmlhttp.addEventListener("load", function(){ 
-    var listaValores = JSON.parse(xmlhttp.responseText);
-    console.log(listaValores);
-    listaValores.forEach(valor => {
-        //console.log(listaValores[0].dataGasValor);
+
+var nivel = null;
+function loadData(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "http://localhost:8080/data-gas");
+    xmlhttp.setRequestHeader("Cache-Control", "no-cache");
+    xmlhttp.setRequestHeader("Pragma", "no-cache");
+    xmlhttp.send();
+    xmlhttp.addEventListener("load", function(){ 
+        var listaValores = JSON.parse(xmlhttp.responseText);
+        console.log(listaValores);
+        listaValores.sort(function(a, b) {
+            return new Date(b.registrationDate) - new Date(a.registrationDate);
+        });
         nivel = parseFloat(listaValores[0].dataGasValor);
+        var valoresTeste = [100, 98, 93, 67, 65, 61, 48, 40, 38, 23, 13, 3];
+        utilizarValor(nivel);
+        var ultimoValor = 0;
+        if(ultimoValor != nivel){
+            ultimoValor = nivel;
+        }
         
     });
-    utilizarValor(nivel);
-});
-*/
-var nivel;
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "http://localhost:8080/data-gas");
-xmlhttp.setRequestHeader("Cache-Control", "no-cache");
-xmlhttp.setRequestHeader("Pragma", "no-cache");
-xmlhttp.send();
-xmlhttp.addEventListener("load", function(){ 
-    var listaValores = JSON.parse(xmlhttp.responseText);
-    console.log(listaValores);
-    listaValores.sort(function(a, b) {
-        return new Date(b.registrationDate) - new Date(a.registrationDate);
-    });
-    nivel = parseFloat(listaValores[0].dataGasValor);
-    utilizarValor(nivel);
-});
+
+}
+
+loadData();
 
 console.log(nivel);
+
+
+
+
+
+
+var btnOk = document.querySelector(".ok");
+var tique = document.getElementById("tique-verde");
+
+
+var btnSolicitaBotj = document.getElementById("solicita");
+btnSolicitaBotj.style.visibility = "hidden";
+
+var bordaCard = document.getElementById("card");
+
+
 
 function utilizarValor(valor){
     var texto = document.querySelector(".texto");
@@ -64,15 +77,6 @@ function utilizarValor(valor){
     
 }
 
-var btnOk = document.querySelector(".ok");
-var tique = document.getElementById("tique-verde");
-
-
-var btnSolicitaBotj = document.getElementById("solicita");
-btnSolicitaBotj.style.visibility = "hidden";
-
-var bordaCard = document.getElementById("card");
-
 
 var modoEscuro = true;
 
@@ -94,6 +98,152 @@ function alternarModo(){
 
     }
 }
+
+
+var right = 105;
+var mediab1 = document.querySelector(".mediab1");
+var calculoMedia;
+var valores = [2, 1.2, 0.7, 0.5, 1.2, 1.5, 0.7, 0.3, 1.8, 2.2, 2, 1.8, 1.9, 0.9, 1, 1.1, 0.8, 1.2, 0.3, 0.4, 0.4, 0.3, 0.2, 0.2, 0.7, 0.9, 1.2, 1.4, 1.2, 1];
+var soma = 0;
+for(var i = 0;i<valores.length; i++){
+    soma+=valores[i];
+}
+console.log(soma);
+var quantidade = valores.length;
+var mediaValores = 0;
+mediaValores = soma/quantidade;
+console.log(mediaValores);
+
+mediab1.textContent = mediaValores.toFixed(2) + " Kg ao dia";
+
+//var pressao = [6, 5.8, 5.5, 4.5, 4, 3.5, 3, 2.5, 1.5, 0];
+var chartDom = document.getElementById('grafico-bom');
+var myChart = echarts.init(chartDom, 'dark');
+var option;
+
+
+const colorsDark = ['rgba(255, 160, 30, 1)', 'rgba(0, 100, 255, 1)', 'rgba(46,209,0,1)'];
+const colorsLight = [];
+
+option = {
+    title: {
+        text: "Gráfico de consumo - Janeiro",
+        left: 'center',
+    },
+  backgroundColor: '',
+  color: colorsDark,
+  toolbox: {
+    top: 20,
+    right: right,
+    itemSize: 20,
+    show: true,
+    feature: {
+      dataZoom: {
+        yAxisIndex: 'none'
+      },
+      dataView: { readOnly: false },
+      magicType: { type: ['line', 'bar', 'stack'] },
+      saveAsImage: {}
+    }
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+    }
+  },
+  legend: {
+    top: 40,
+    textStyle: {color: 'white'},
+  },
+  grid: {
+    top: 70,
+    bottom: 50
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      axisTick: {
+        alignWithLabel: true
+      },
+      axisLine: {
+        lineStyle: {
+        color: 'rgb(0, 0, 0)'
+        }
+      },
+      data: ['01/1', '02/1', '03/01', '04/01', '05/01', '06/01', '07/01', '08/01', '09/01', '10/01', '11/01', '12/01', '13/01', '14/01', '15/01', '16/01', '17/01', '18/01', '19/01', '20/01', '21/01', '22/01', '23/01', '24/01', '25/01', '26/01', '27/01', '28/01', '29/01', '30/01']
+    },
+  ],
+  
+  yAxis:
+    {
+      
+      type: 'value',
+      axisLabel: {
+        formatter: '{value} Kg',
+      }
+      
+    },
+    
+  series: [
+    {
+      stack: 'Total',  
+      symbolSize: 7,
+      color: colorsDark[1],
+      name: 'Consumo Total p/dia',
+      type: 'line',
+      markPoint: {
+        data: [
+          { type: 'max', name: 'Max', symbolSize: 40 },
+        ]
+      },
+      smooth: true,
+      emphasis: {
+        focus: 'series'
+      },
+      
+      areaStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgba(58,150,233,0.8)'
+          },
+          {
+            offset: 1,
+            color: 'rgba(0,40,255,0.3)'
+          }
+        ])
+      },
+      data: valores,
+    },
+    
+    /*
+    {
+      name: 'Gás 3',
+      type: 'line',
+      markPoint: {
+        data: [
+          { type: 'max', name: 'Max' },
+        ]
+      },
+      smooth: true,
+      emphasis: {
+        focus: 'series'
+      },
+      
+      
+      data: [
+        2.9, 4.9, 10.1, 45.7, 0, 220, 231.6, 46.6, 55.4, 18.4, 10.3, 0.7
+      ]
+      
+    },
+    */
+    
+  ]
+};
+
+option && myChart.setOption(option);
 
 
 
